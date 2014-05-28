0b2a1ef80a875fed8fedbe80f0238c722da92485
somtomking <liust@foxmail.com>
2014-05-28 12:57:31 +0800

    添加web的测试项目

---
+++ Tests/Nop.Web.Test/ControllerTestBase.cs
@@ -0,0 +1,45 @@
+﻿using Nop.Tests;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Web.Test
+{
+     [TestFixture]
+    public class ControllerTestBase : TestsBase
+    {
+          /// <summary>
+        /// 测试类执行之前
+        /// </summary>
+        [TestFixtureSetUp]
+        public virtual void TestFixtureSetUp()
+        {
+
+        }
+        /// <summary>
+        /// 测试类执行之后
+        /// </summary>
+        [TestFixtureTearDown]
+        public virtual void TestFixtureTearDown()
+        {
+
+        }
+         /// <summary>
+         /// 方法执行之前
+         /// </summary>
+        public override void SetUp()
+        {
+
+        }
+         /// <summary>
+         /// 方法执行之后
+         /// </summary>
+        public override void TearDown()
+        {
+
+        }
+    }
+}
---
+++ Tests/Nop.Web.Test/ShoppingCartControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Web.Controllers;
+using Nop.Tests;
+using NUnit.Framework;
+using Nop.Core.Infrastructure;
+namespace Nop.Web.Test
+{
+    [TestFixture]
+    public class ShoppingCartControllerTests : ControllerTestBase
+    {
+        public override void TestFixtureSetUp()
+        {
+          EngineContext.Initialize(false);
+
+
+        }
+        public override void TestFixtureTearDown()
+        {
+
+        }
+        public override void SetUp()
+        {
+
+        }
+        public override void TearDown()
+        {
+
+        }
+        [Test]
+        public void Test()
+        {
+            var controller = EngineContext.Current.Resolve<ShoppingCartController>();
+            
+        }
+    }
+}
