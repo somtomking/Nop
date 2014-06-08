1b9c9c37133c2290c1f4622394aedc5dcf68dbcd
somtomking <somtomking@163.com>
2014-06-08 12:57:06 +0800

    temai

--- Plugins/Nop.Plugin.Widgets.ProductSpecialSale/Models/SpecialSaleStageModel.cs
+++ Plugins/Nop.Plugin.Widgets.ProductSpecialSale/Models/SpecialSaleStageModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +14,13 @@ namespace Nop.Plugin.Widgets.ProductSpecialSale.Models
         [DisplayName("特卖名称")]
         public string Name { get; set; }
         [DisplayName("特卖开始时间")]
-        public DateTime StartTime { get; set; }
+        [UIHint("DateTimeNullable")]
+        public DateTime? StartTime { get; set; }
         [DisplayName("特卖结束时间")]
-        public DateTime EndTime { get; set; }
+        [UIHint("DateTimeNullable")]
+        public DateTime? EndTime { get; set; }
         [DisplayName("特卖显示顺序")]
+        [UIHint("Int32Nullable")]
         public int DisplayOrder { get; set; }
     }
 }
