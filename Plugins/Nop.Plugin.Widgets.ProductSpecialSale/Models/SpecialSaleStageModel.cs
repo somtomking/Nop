f038c520e7efe30cea5f22dd707271977f7596d5
somtomking <somtomking@163.com>
2014-06-08 09:48:15 +0800

    temai

--- Plugins/Nop.Plugin.Widgets.ProductSpecialSale/Models/SpecialSaleStageModel.cs
+++ Plugins/Nop.Plugin.Widgets.ProductSpecialSale/Models/SpecialSaleStageModel.cs
@@ -1,6 +1,7 @@
 ﻿using Nop.Web.Framework.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace Nop.Plugin.Widgets.ProductSpecialSale.Models
 {
     public class SpecialSaleStageModel : BaseNopEntityModel
     {
+        [DisplayName("特卖名称")]
         public string Name { get; set; }
+        [DisplayName("特卖开始时间")]
         public DateTime StartTime { get; set; }
+        [DisplayName("特卖结束时间")]
         public DateTime EndTime { get; set; }
+        [DisplayName("特卖显示顺序")]
         public int DisplayOrder { get; set; }
     }
 }
